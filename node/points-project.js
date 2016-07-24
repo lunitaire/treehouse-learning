718e1a59e70657d82bc6a0967a244a4193028a16
Lunitaire <atrian@lunitaire.com>
2016-07-24 19:56:31 -0400

    Updating points-project.js to print out total badges and points in JS

--- node/points-project.js
+++ node/points-project.js
@@ -3,19 +3,31 @@
 var https = require("https");
 var username = "atrianwagner";
 
+//Print out message
 function printMessage(username, badgeCount, points) {
     var message = username + " has " + badgeCount + " total badge(s) and " + points + " points in JavaScript";
     console.log(message);
 }
 
+//Print out error messages
+function printError(error){
+    console.error(error.message);
+}
+
 //Connect to the API URL. (https://teamtreehouse.com/username.json)
 
 var request = https.get("https://teamtreehouse.com/" + username + ".json", function(response){
-    console.log(response.statusCode);
+    //console.log(response.statusCode);
+    var body = "";
 
 //Read the data.
+
     response.on('data', function(chunk) {
-        console.log('BODY: '+ chunk);
+        body += chunk;
+    });
+    response.on('end', function(){
+        var profile = JSON.parse(body);
+        printMessage(username, profile.badges.length, profile.points.JavaScript);
     });
 
 //Parse the data.
@@ -24,7 +36,8 @@ var request = https.get("https://teamtreehouse.com/" + username + ".json", funct
 });
 
 
-//Catch any errors.
-request.on("error", function(error){
-    console.error(error.message);
-});
+//Connection Error
+request.on("error", printError);
+
+
+//Extras: jsonprettyprint.com
