3de550f74f32f329da83449dd289c4bab15372ac
Lunitaire <atrian@lunitaire.com>
2016-07-24 21:55:36 -0400

    Added multiple users.

--- node/display-userinfo.js
+++ node/display-userinfo.js
@@ -1,3 +1,7 @@
 //This is for points-project.js
 var profile = require("./points-project.js");
-profile.get("atrianwagner");
+//console.dir(process.argv);
+//var users = process.argv.slice(2);
+var users = ["chalkers", "atrianwagner", "nickpettit", "davemcfarland"];
+
+users.forEach(profile.get);
