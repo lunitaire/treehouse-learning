ca2dadf85cee9418d38f3b75b5b08e1b1c8bfda2
awagner <awagner@aptosolutions.com>
2016-07-25 12:02:28 -0400

    tasklist started to add Event Handling

--- javascript/README.md
+++ javascript/README.md
@@ -3,10 +3,12 @@
 This folder will contain my JS specific projects from Treehouse.
 
 #### Track Notes
-* #####DOM actions
-   * __Manipulation__ - where we manipulate elements
-   * __Traversal__ - selecting an element based on the relationship with another element
-   * __Events__ - listening to a specific event, like a mouse click or a key press, and having something execute in response
+Team Treehouse notes on Full Stack JavaScript Track
+
+####DOM actions
+* __Manipulation__ - where we manipulate elements
+* __Traversal__ - selecting an element based on the relationship with another element
+* __Events__ - listening to a specific event, like a mouse click or a key press, and having something execute in response
 
 #### References
 * [Mozilla Developer Network Web API](https://developer.mozilla.org/en-US/docs/Web/Reference/API)
--- javascript/tasklist/js/app.js
+++ javascript/tasklist/js/app.js
@@ -8,6 +8,7 @@ var completedTasksHolder = document.getElementById("completed-task"); //complete
 
 //Add a new task
 var addTask = function() {}
+    console.log("Add Task");
     //When the button is pressed
     //Create a new list item with the text from #new-task:
         //input (checkbox)
@@ -20,6 +21,7 @@ var addTask = function() {}
 
 //Edit an existing task
 var editTask = function() {
+	console.log("Edit Task");
     //When the Edit button is pressed
         //if the class of the parent is .editMode
             //switch from .editMode
@@ -34,19 +36,40 @@ var editTask = function() {
 
 //Delete an existing task
 var deleteTask = function() {
+	console.log("Delete Task");
     //When the Delete button is pressed
         //Remove the parent list item from the ul
 }
 
 //Mark a task as complete
 var taskCompleted = function() {
+	console.log("Task Complete");
     //When the checkbox is checked
         //Append the task list item to the #completed-tasks
 }
 
 //Mark a task as incomplete
 var taskIncomplete = function () {
+	console.log("Task Incomplete");
     //When the checkbox is unchecked
         //Append the task list item to the #incomplete-tasks
 }
 
+var bindTaskEvents(taskListItem, checkboxEventHandler) {
+    //select its children
+    //bind editTask to edit button
+    //bind deleteTask to the delete button
+    //bind checkboxEventHandler to checkbox
+
+}
+
+//Set the click handler to the addTask function
+addButton.onclick = addTask;
+
+//cycle over incompleteTasksHolder ul list items
+    //for each list item
+        //bind events to list item's children (taskCompleted)
+
+//cycle over completeTasksHolder ul list items
+    //for each list item
+        //bind events to list item's children (taskIncompleted)
