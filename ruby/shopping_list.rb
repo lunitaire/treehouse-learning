b4129fc3a3785d2b75e752e5a2b402726980d73e
awagner <awagner@aptosolutions.com>
2016-07-28 15:29:00 -0400

    Adding an item to shopping_list.rb

--- ruby/shopping_list.rb
+++ ruby/shopping_list.rb
@@ -1,7 +1,7 @@
 # Shopping List
 
 def create_list
-  print "What is the list name?"
+  print "What is the list name? "
   name = gets.chomp
 
   hash = {"name" => name, "items" => Array.new }
@@ -9,14 +9,18 @@ def create_list
 end
 
 def add_list_item
-  print "What is the item called?"
+  print "What is the item called? "
   item_name = gets.chomp
+  
+  print "How much? "
+  quantity = gets.chomp.to_i
 
-  hash = {"name" => name}
+  hash = {"name" => item_name, "quantity" => quantity}
   return hash
 end
 
 list = create_list()
 puts list.inspect
+list['items'].push(add_list_item())
 
-puts add_list_item().inspect
+puts lists.inspect
