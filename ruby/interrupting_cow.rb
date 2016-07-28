c8ec1e857a0609d59ee661f126de8ea220000187
awagner <awagner@aptosolutions.com>
2016-07-28 16:52:58 -0400

    Updated shopping_list.rb to intake multiple items, and create interrupting_cow.rb.

---
+++ ruby/interrupting_cow.rb
@@ -0,0 +1,21 @@
+# loop interrupting cow
+phrases = ["Knock. ","Knock. \n","Who's ","there? \n", "Interrupting ", "cow. \n", "Interrupting ", "cow ", "who? \n"]
+numphrase = 0
+
+print "Let's hear a joke! \n"
+print "You know how to stop me if you've heard this one before. \n"
+
+loop do 
+  print phrases[numphrase]
+  answer = gets.chomp.downcase
+  if (answer == "moo") && (numphrase != 8)
+    break
+  elsif (answer == "moo") && (numphrase == 8)
+    print "Poor timing. \n"
+    numphrase = 0
+  elsif (answer != "moo") && (numphrase == 8)
+    numphrase = 0
+  else
+    numphrase += 1
+  end
+end
--- ruby/shopping_list.rb
+++ ruby/shopping_list.rb
@@ -19,23 +19,35 @@ def add_list_item
   return hash
 end
 
+def print_separator(character="-")
+  puts character * 60
+end
+
 def print_list(list) 
   puts "List: #{list['name']}"
-  puts "----"
+  print_separator()
 
   list["items"].each do |item|
-    puts "Item: " +item['name']
-    puts "Quantity: " + item ['quantity'].to_s
-    puts "---"
-  end
+    puts "\tItem: " + item['name'] +"\t\t\t" +
+         "Quantity: " + item ['quantity'].to_s
+    end
+  
+  print_separator()
 end
 
 list = create_list()
-puts list.inspect
-list['items'].push(add_list_item())
 
-puts list.inspect
+def itemloop(list) 
+  print "How many items would you like to add? "
+  itemnum = gets.chomp.to_i
+  i = 0
+  while i < itemnum
+    list['items'].push(add_list_item())
+    i += 1 
+  end
+end
 
+itemloop(list)
 
 print_list(list)
 
