4c4ceff77329c2dedece18251993d1003f547ac6
awagner <awagner@aptosolutions.com>
2016-07-29 15:15:17 -0400

    Added number method to litle_black_book.rb.

--- ruby/little_black_book.rb
+++ ruby/little_black_book.rb
@@ -8,3 +8,28 @@ def ask (question, kind="string")
   answer = answer.to_i if kind == "number"
   return answer
 end
+
+def add_contact
+  contact = {"name" => "", "phone_number" => []}
+  contact["name"] = ask("What is the person's name?")
+  answer = ""
+  while answer != "n"
+    answer = ask("Do you want to add a phone number? (y/n)")
+    if answer == "y"
+      phone = ask("Enter a phone number: ")
+      contact["phone_numbers"].push(phone) 
+    end
+  end
+  return contact
+end
+
+answer = ""
+while answer != "n"
+  contact_list.push(add_contact())
+  answer = ask("Add another? (y/n)")
+end
+
+
+#answer = ask("What is your name?")
+#puts answer
+
