4cb9b2226310d1a74a59e5a0bfb3676b82c0277d
George Spiceland <georgespiceland@gmail.com>
2016-07-29 16:21:07 -0400

    Update little_black_book.rb
    Resolved typo in hash declaration, swapped hash key strings with symbols

--- ruby/little_black_book.rb
+++ ruby/little_black_book.rb
@@ -10,14 +10,18 @@ def ask (question, kind="string")
 end
 
 def add_contact
-  contact = {"name" => "", "phone_number" => []}
-  contact["name"] = ask("What is the person's name?")
+  contact = {:name => "", :phone_numbers => []} #Fixed a typo in the declaration, also
+                                                #Ruby hashes should use symbols, not strings for keys
+                                                #Symbols are only stored once, conserving memory and increasing speed
+                                                #While strings must be stored continously. This also fixes the key as immutable
+                                                #or unchanging, which is obviously needed for hashes to behave reliably
+  contact[:name] = ask("What is the person's name?")
   answer = ""
   while answer != "n"
     answer = ask("Do you want to add a phone number? (y/n)")
     if answer == "y"
       phone = ask("Enter a phone number: ")
-      contact["phone_numbers"].push(phone) 
+      contact[:phone_numbers].push(phone)
     end
   end
   return contact
@@ -32,14 +36,13 @@ end
 puts "---"
 
 contact_list.each do |contact|
-  puts "Name: #{contact["name"]}"
-  if contact["phone_numbers"].size > 0
-    contact["phone_numbers"].each do |phone_number|
+  puts "Name: #{contact[:name]}"
+  if contact[:phone_numbers].size > 0
+    contact[:phone_numbers].each do |phone_number|
       puts "Phone: #{phone_number}"
     end
   end
   puts "-----\n"
 end
-
 
 
