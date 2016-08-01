ad8b5e441178b09c7c9d3b3c633f559ae24689a2
awagner <awagner@aptosolutions.com>
2016-07-31 22:16:06 -0400

    Creating transactions in bank_account.rb.

--- ruby/bank_account.rb
+++ ruby/bank_account.rb
@@ -1,11 +1,30 @@
 # Treehouse Bank Account Project
 class BankAccount
+  attr_reader :name
+  
+
   def initialize(name)
     @name = name
     @transactions = []
+    add_transaction("Beginning Balance", 0)
   end
+
+  def credit(description, amount)
+    add_transaction(description, amount)
+  end
+
+  def debit(description, amount)
+    add_transaction(description, -amount)
+  end
+
+  def add_transaction(description, amount)
+    @transactions.push(description: description, amount: amount)
+  end
+
 end
 
 
 bank_account = BankAccount.new("Jason")
-put bank_account.inspect
+bank_account.credit("Opening Deposit", 500)
+bank_account.debit("Groceries", 40)
+puts bank_account.inspect
