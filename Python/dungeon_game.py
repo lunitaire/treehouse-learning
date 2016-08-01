ef04a8448198cede46a00aec1e00a2badfb26d91
awagner <awagner@aptosolutions.com>
2016-08-01 14:31:02 -0400

    Fleshed out the basics for dungeon_game.py.

--- Python/dungeon_game.py
+++ Python/dungeon_game.py
@@ -1,3 +1,4 @@
+# Requires Python3
 import random
 
 CELLS = [(0, 0), (0, 1), (0, 2),
@@ -6,38 +7,61 @@ CELLS = [(0, 0), (0, 1), (0, 2),
 
 
 def get_locations():
-    # monster = random
-    # door = random
-    # start = random
+    monster = random.choice(CELLS)
+    door = random.choice(CELLS)
+    start = random.choice(CELLS)
 
     # if monster, door, or start are the same, do it again
+    if monster == door or monster == start or door == start:
+        return get_locations
 
-    #return monster, door, start
-    pass
+    return monster, door, start
 
 
 def move_player(player, move):
     # Get the player's current location
+    x, y = player
+
     # If move is LEFT, y - 1
+    if move == 'LEFT':
+        y -=1
     # If move is RIGHT, y + 1
+    if move == 'RIGHT':
+        y +=1
     # If move is UP, x - 1
+    if move == 'UP':
+        x -=1
     # If move is DOWN, x + 1
-    return player
+    if move == 'DOWN':
+        x =1
+    return x, y
 
 
 def get_moves(player):
-    MOVES = ['LEFT', 'RIGHT', 'UP', 'DOWN']
+    moves = ['LEFT', 'RIGHT', 'UP', 'DOWN']
+    # player = (x,y)
     # if player's y is 0, remove LEFT
+    if player[1] == 0:
+        moves.remove('LEFT')
     # if player's x is 0, remove UP
+    if player[1] == 2:
+        moves.remove('RIGHT')
     # if player's y is 2, remove RIGHT
+    if player[0] == 0:
+        moves.remove('UP')
     # if player's x is 2, remove DOWN
-    return MOVES
+    if player[0] == 2:
+        moves.remove('DOWN')
+    return moves
 
+monster, door, player = get_locations()
 
 while True:
+    moves = get_moves(player)
+
     print("Welcome to the dungeon!")
-    print("You're currently in room {}")  # fill in with player position
-    print("You can move {}")  # fill in with available moves
+    print("You're currently in room {}".format(player))
+    print("You can move {}".format(moves))
     print("Enter QUIT to quit")
 
     move = input("> ")
@@ -47,7 +71,18 @@ while True:
         break
     
     # If it's a good move, change the player's position
+    if move in moves:
+        player = move_player(player, move)
     # If it's a bad move, don't change anything
+    else:
+        print("** Walls are hard; stop walking into them! **")
+        continue
     # If the new player position is the door, they win!
+    if player == door:
+        print ("You escaped!")
+        break
     # If the new player position is the monster, they lose!
+    elif player == monster:
+        print ("You were eaten by the grue!")
+        break
     # Otherwise, continue
