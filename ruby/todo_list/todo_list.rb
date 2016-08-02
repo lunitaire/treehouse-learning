174b3d6ff84191e59b7ebadc90e0f0499b8ca3bb
awagner <awagner@aptosolutions.com>
2016-08-02 18:31:26 -0400

    Added add_item and remove_item to monster_battle.

--- ruby/todo_list/todo_list.rb
+++ ruby/todo_list/todo_list.rb
@@ -7,6 +7,31 @@ class TodoList
         @name = name
         @todo_items = []
     end
+
+    def add_item(name)
+        todo_items.push(TodoItem.new(name))
+    end
+
+    def remove_item(name)
+        index = 0
+        found = false
+        todo_items.each do |todo_item|
+            if todo_item.name == name
+                found = true
+            end
+            if found
+                break
+            else
+                index += 1
+            end
+            if found
+                todo_items.delete_at(index)
+                return true
+            else
+                return false
+            end
+        end
+    end
 end
 
 todo_list = TodoList.new("Art Projects")
