a2c3ed884cc8e33b17ca5c681cb92393086cc899
Lunitaire <atrian@lunitaire.com>
2016-08-05 23:46:00 -0400

    Added last_first method to rolodex.

--- Ruby/rolodex/contact.rb
+++ Ruby/rolodex/contact.rb
@@ -13,6 +13,16 @@ class Contact
     @last_name
   end
 
+  def last_first
+    last_first = last_name
+    last_first += ", "
+    last_first += first_name
+    if !@middle_name.nil?
+      last_first += " "
+      last_first += middle_name.slice(0, 1)
+      last_first += "."
+  end
+
   def full_name
     full_name = first_name
     if !@middle_name.nil?
