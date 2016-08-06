0f969e34836e50b97c220887ec6b857b4252f943
Lunitaire <atrian@lunitaire.com>
2016-08-06 00:14:55 -0400

    Adding to_s for address.rb for rolodex.

--- Ruby/rolodex/address.rb
+++ Ruby/rolodex/address.rb
@@ -1,5 +1,17 @@
 class Address
   attr_accessor :kind, :street_1, :street_2, :city, :state, :postal_code
 
-
+  def to_s(format = 'short')
+    address = ''
+    case format
+    when 'short'
+    	address += "#{kind}: "
+    	address += street_1
+    	if street_2
+    		address += " " + street_2
+    	end
+    	address += ", #{city}, #{state}, #{postal_code}"
+    end
+    address
+  end
 end
