39fbff2dcf9252543c924d6894925f12a95eb223
Lunitaire <atrian@lunitaire.com>
2016-08-05 23:41:36 -0400

    Starting Treehouse Ruby project Address Book aka rolodex.

---
+++ Ruby/rolodex/contact.rb
@@ -0,0 +1,27 @@
+class Contact
+  attr_writer :first_name, :middle_name, :last_name
+
+  def first_name
+    @first_name
+  end
+
+  def middle_name
+    @middle_name
+  end
+
+  def last_name
+    @last_name
+  end
+
+  def full_name
+    full_name = first_name
+    if !@middle_name.nil?
+      full_name += " "
+      full_name += middle_name
+    end
+    full_name += " "
+    full_name += last_name
+    full_name
+  end
+end
+
