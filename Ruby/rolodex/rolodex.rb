650b24fe9f97f3d3b13a81630908d1638fc5f84a
Lunitaire <atrian@lunitaire.com>
2016-08-09 20:07:18 -0400

    Filling out the rolodex.

--- Ruby/rolodex/rolodex.rb
+++ Ruby/rolodex/rolodex.rb
@@ -1,3 +1,5 @@
+require "./contact"
+
 class Rolodex
   attr_reader :contacts
 
@@ -6,8 +8,29 @@ class Rolodex
   end
 
   def print_contact_list
+  	puts "Contact List"
   	contacts.each do |contact|
   	  puts contact.to_s('last_first')
   	end
   end
 end
+
+address_book = Rolodex.new
+
+jason = Contact.new
+jason.first_name = "Jason"
+jason.last_name = "Seifer"
+jason.add_phone_number("Home", "123-456-7890")
+jason.add_phone_number("Work", "456-789-0123")
+jason.add_address("Home", "123 Main St.", "", "Portland", "OR", "12345")
+
+nick = Contact.new
+nick.first_name = "Nick"
+nick.last_name = "Pettit"
+nick.add_phone_number("Home", "222-222-2222")
+nick.add_address("Home", "222 Two Lane", "", "Portland", "OR", "12345")
+
+address_book.contacts.push(jason)
+address_book.contacts.push(nick)
+
+address_book.print_contact_list
