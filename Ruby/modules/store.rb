821928ba518b17c4c9ad19bbb6c8182c868129ba
awagner <awagner@aptosolutions.com>
2016-08-11 17:18:11 -0400

    Created in_stock_report for store.rb in Ruby Modules.

--- Ruby/modules/store.rb
+++ Ruby/modules/store.rb
@@ -2,6 +2,7 @@ module Inventoryable
 
     def self.included(klass)
         klass.extend(ClassMethods)
+        klass.extend(Enumerable)
     end
 
     module ClassMethods
@@ -13,6 +14,25 @@ module Inventoryable
 
         def instances
             @instances ||= []
+        end
+
+        def each(&block)
+            instances.each(&block)
+        end
+
+        def in_stock_report
+            puts "#{self.to_s} In Stock Report"
+            reportable = instances.select{ |instance| instance.in_stock? }
+            reportable.each do |item|
+                line = []
+                line.push("Item #{item.attributes[:name]}")
+                line.push("Stock: #{item.stock_count}")
+                if item.attributes.include?(:size)
+                    line.push("Size: #{item.attributes[:size]}")
+                end
+                puts line.join("\t")
+            end
+            puts "\n"
         end
     end
 
@@ -57,9 +77,21 @@ class Accessory
 end
 
 shirt1 = Shirt.create(name: "MTF", size: "L")
-shirt2 = Shirt.create(name: "MTF", size: "M")
-
 shirt1.stock_count = 10
 
-puts "Shirt 1 stock count: %s" % shirt1.stock_count
-puts "Shirt 2 stock count: %s" % shirt2.stock_count
+shirt2 = Shirt.create(name: "MTF", size: "M")
+shirt2.stock_count = 4
+
+shirt1 = Pants.create(name: "Frog Joggers", size: "XL")
+shirt1.stock_count = 8
+
+shirt2 = Pants.create(name: "Frog Joggers", size: "M")
+shirt2.stock_count = 6
+
+shirt1 = Accessory.create(name: "Bracelet", size: "L")
+shirt1.stock_count = 1
+
+shirt2 = Accessory.create(name: "Belt", size: "S")
+shirt2.stock_count = 3
+
+Shirt.in_stock_report
