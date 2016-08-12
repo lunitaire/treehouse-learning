21f0366f45823d2758637d149ef4b65aeb298eea
awagner <awagner@aptosolutions.com>
2016-08-12 15:15:52 -0400

    File class practice in Ruby.

---
+++ Ruby/file.rb
@@ -0,0 +1,10 @@
+File.open("testfile.txt", "a+") do |file|
+    print "Enter your name? "
+    name = gets.chomp
+    file.puts "Name: #{name}"
+
+    print "Enter your email: "
+    email = gets.chomp
+    file.puts "Email: #{email}"
+
+end
