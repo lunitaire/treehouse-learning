b47a26d23a2f9ea2cfa7bf03b07f0c4d81ad2db6
awagner <awagner@aptosolutions.com>
2016-08-23 12:37:00 -0400

    Building out userRoute for profile_project.

--- JavaScript/Nodejs/profile_project/app.js
+++ JavaScript/Nodejs/profile_project/app.js
@@ -19,21 +19,32 @@ server.listen(port, hostname, () => {
 // 2. Handle HTTP route GET / and POST / i.e. Home
 function homeRoute(request, response) {
     // if url == "/" && GET
+    if(request.url === "/"){
         // show search
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.write("Header\n");
         response.write("Search\n");
         response.end('Footer\n');
+    }
     // if url == "/" && POST
         // redirect to /:username
 }
 // 3. Handle HTTP route GET /:username i.e. /chalkers
+function userRoute(request, response){
     // if url == "/...."
+    var username = request.url.replace("/", "");
+    if(username.length > 0) {
         // get json from Treehouse
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write("Header\n");
+        response.write(username + "\n");
+        response.end('Footer\n');
             // on "end"
                 // show profile
             // on "error"
                 // show error
+    }
+}
 
 // 4. Function that handles the reading of files and merge in value
     // read from file and get a string
